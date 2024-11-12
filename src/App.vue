90d4734903d90f3b0338c330b5a6c3c41ac0ef07
Joelle <jtindall222@gmail.com>
2024-11-12 16:30:04 -0500

    Add files via upload

--- src/App.vue
+++ src/App.vue
@@ -12,22 +12,19 @@ const route = useRoute()
 const windowWidth = ref(window.innerWidth)
 const windowHeight = ref(window.innerHeight)
 
-
 watch(windowWidth, () => {
   // Do something when window width changes....
   // sets mobile menu to false when screen wide enough, so body can be scrolled
   if (windowWidth.value > 768) {
-    menu.value = false;
+    menu.value = false
   }
 })
-watch(windowHeight, () => {
-
-})
+watch(windowHeight, () => {})
 
 // Update windowWidth on resize
 window.addEventListener('resize', () => {
-  windowWidth.value = window.innerWidth;
-  windowHeight.value = window.innerHeight;
+  windowWidth.value = window.innerWidth
+  windowHeight.value = window.innerHeight
 })
 
 // returns the class for the contentwrapper div based on the URL
@@ -40,9 +37,7 @@ function whichPage() {
   }
 }
 
-
-
-// determines the class for the input button 
+// determines the class for the input button
 
 function showPage(page_selected) {
   // Reset all pages
@@ -51,14 +46,11 @@ function showPage(page_selected) {
   window.scrollTo(0, 0)
 
   if (page_selected === 'about') {
-
     page.value = 1
     pageOpen.value[0] = true
-
   } else if (page_selected === 'projects') {
     page.value = 2
     pageOpen.value[1] = true
-
   } else if (page_selected === 'contact') {
     page.value = 3
     pageOpen.value[2] = true
@@ -66,52 +58,48 @@ function showPage(page_selected) {
     page.value = 4
     pageOpen.value[3] = true
   }
-  menu.value = false;
+  menu.value = false
 }
 
 function showMenu() {
   if (!menu.value) {
-    menu.value = true;
+    menu.value = true
   } else {
-    menu.value = false;
+    menu.value = false
   }
 }
 
-
 function comicSans() {
   if (!comic.value) {
-    comic.value = true;
+    comic.value = true
   } else {
-    comic.value = false;
+    comic.value = false
   }
 }
 
 function logoWink() {
   if (!wink.value) {
-    wink.value = true;
+    wink.value = true
   } else {
-    wink.value = false;
+    wink.value = false
   }
 }
 
 //disable scrolling when menu open
-watch(menu, (newValue) => {
+watch(menu, newValue => {
   if (newValue) {
     document.body.style.overflow = 'hidden'
   } else {
     document.body.style.overflow = 'initial'
   }
 })
-
 </script>
-
 
 <style>
 @import './assets/main.css';
 </style>
 
 <template>
-
   <div class="topheader" :class="{ expand: menu }">
     <div class="rainleft">
       <div class="raina"></div>
@@ -128,46 +116,109 @@ watch(menu, (newValue) => {
     </div>
 
     <div class="title">
-      <img v-show="!wink" class="logo" :class="{ expand: menu }" src="./assets/images/joelle.png"
-        @click.prevent="logoWink" />
-      <img v-show="wink" class="logo" :class="{ expand: menu }" src="./assets/images/joellewink.png"
-        @click="logoWink" />
-
+      <img
+        v-show="!wink"
+        class="logo"
+        :class="{ expand: menu }"
+        src="./assets/images/joelle.png"
+        @click.prevent="logoWink"
+      />
+      <img
+        v-show="wink"
+        class="logo"
+        :class="{ expand: menu }"
+        src="./assets/images/joellewink.png"
+        @click="logoWink"
+      />
     </div>
     <div class="mobmenubtn">
-      <img class="hamburger" v-show="!menu" src="./assets/images/hamburger.png" @click="showMenu" />
-      <img class="hamburger" v-show="menu" src="./assets/images/hamburgerx.png" @click="showMenu" />
+      <img
+        class="hamburger"
+        v-show="!menu"
+        src="./assets/images/hamburger.png"
+        @click="showMenu"
+      />
+      <img
+        class="hamburger"
+        v-show="menu"
+        src="./assets/images/hamburgerx.png"
+        @click="showMenu"
+      />
     </div>
     <div class="mobilemenu" :class="{ expand: menu }">
-      <input id="about" class="am" @click="$router.push({ name: 'About' }, showPage('about'))"
-        :class="{ open: pageOpen[0], closed: !pageOpen[0] }" type="button" value="About" />
-      <input id="projects" class="bm" @click="$router.push({ name: 'Projects' }, showPage('projects'))"
-        :class="{ open: pageOpen[1], closed: !pageOpen[1] }" type="button" value="Projects" />
-      <input id="contact" class="cm" @click="$router.push({ name: 'Contact' }, showPage('contact'))"
-        :class="{ open: pageOpen[2], closed: !pageOpen[2] }" type="button" value="Contact" />
-      <input id="guestbook" class="dm" @click="$router.push({ name: 'Guestbook' }, showPage('guestbook'))"
-        :class="{ open: pageOpen[3], closed: !pageOpen[3] }" type="button" value="Guestbook" />
-
+      <input
+        id="about"
+        class="am"
+        @click="$router.push({ name: 'About' }, showPage('about'))"
+        :class="{ open: pageOpen[0], closed: !pageOpen[0] }"
+        type="button"
+        value="About"
+      />
+      <input
+        id="projects"
+        class="bm"
+        @click="$router.push({ name: 'Projects' }, showPage('projects'))"
+        :class="{ open: pageOpen[1], closed: !pageOpen[1] }"
+        type="button"
+        value="Projects"
+      />
+      <input
+        id="contact"
+        class="cm"
+        @click="$router.push({ name: 'Contact' }, showPage('contact'))"
+        :class="{ open: pageOpen[2], closed: !pageOpen[2] }"
+        type="button"
+        value="Contact"
+      />
+      <input
+        id="guestbook"
+        class="dm"
+        @click="$router.push({ name: 'Guestbook' }, showPage('guestbook'))"
+        :class="{ open: pageOpen[3], closed: !pageOpen[3] }"
+        type="button"
+        value="Guestbook"
+      />
     </div>
 
     <div class="topbuttons buttonholder ma">
-
-      <input id="aboutbtn" type="button" class="a" @click="$router.push({ name: 'About' }, showPage('about'))"
-        :class="{ open: pageOpen[0], closed: !pageOpen[0] }" value="About" />
+      <input
+        id="aboutbtn"
+        type="button"
+        class="a"
+        @click="$router.push({ name: 'About' }, showPage('about'))"
+        :class="{ open: pageOpen[0], closed: !pageOpen[0] }"
+        value="About"
+      />
     </div>
     <div class="topbuttons buttonholder mb">
-
-      <input id="projectsbtn" type="button" class="b" @click="$router.push({ name: 'Projects' }, showPage('projects'))"
-        :class="{ open: pageOpen[1], closed: !pageOpen[1] }" value="Projects" />
+      <input
+        id="projectsbtn"
+        type="button"
+        class="b"
+        @click="$router.push({ name: 'Projects' }, showPage('projects'))"
+        :class="{ open: pageOpen[1], closed: !pageOpen[1] }"
+        value="Projects"
+      />
     </div>
     <div class="topbuttons buttonholder mc">
-      <input id="contactbtn" type="button" class="c" @click="$router.push({ name: 'Contact' }, showPage('contact'))"
-        :class="{ open: pageOpen[2], closed: !pageOpen[2] }" value="Contact" />
+      <input
+        id="contactbtn"
+        type="button"
+        class="c"
+        @click="$router.push({ name: 'Contact' }, showPage('contact'))"
+        :class="{ open: pageOpen[2], closed: !pageOpen[2] }"
+        value="Contact"
+      />
     </div>
     <div class="topbuttons buttonholder md">
-      <input id="guestbookbtn" type="button" class="d"
+      <input
+        id="guestbookbtn"
+        type="button"
+        class="d"
         @click="$router.push({ name: 'Guestbook' }, showPage('guestbook'))"
-        :class="{ open: pageOpen[3], closed: !pageOpen[3] }" value="Guestbook" />
+        :class="{ open: pageOpen[3], closed: !pageOpen[3] }"
+        value="Guestbook"
+      />
     </div>
 
     <div class="rainright">
@@ -183,25 +234,24 @@ watch(menu, (newValue) => {
       <div class="rainh"></div>
       <div class="raini"></div>
     </div>
-
   </div>
   <div class="wrapper" :class="{ comicsans: comic }">
-
     <div class="maindiv">
-
-
       <div id="contentwrapper" v-bind:class="whichPage()">
-
         <router-view></router-view>
 
         <div id="Footer">
-          <img class="bouncecat" :class="{ flipped: comic }" :src="Bouncecat" @click="comicSans">
+          <img
+            class="bouncecat"
+            :class="{ flipped: comic }"
+            :src="Bouncecat"
+            @click="comicSans"
+          />
           <p>You've reached the end!</p>
         </div>
       </div>
     </div>
   </div>
-
 </template>
 
 <style>
@@ -211,6 +261,8 @@ watch(menu, (newValue) => {
 #guestbookbtn:hover {
   color: black;
   font-weight: bolder;
-  box-shadow: 0px -1px 10px 2px rgba(0, 0, 0, 0.104), 0 5px rgba(0, 0, 0, 0.107);
+  box-shadow:
+    0px -1px 10px 2px rgba(0, 0, 0, 0.104),
+    0 5px rgba(0, 0, 0, 0.107);
 }
 </style>
--- src/components/Guestbook.vue
+++ src/components/Guestbook.vue
@@ -1,84 +1,125 @@
-<script>
-import { ref, watch } from 'vue'
+<script setup>
+import { ref, watch, onMounted } from 'vue'
+import { supabase } from '@/lib/supabaseClient'
 
-export default {
-  setup() {
-    const submitted = ref(false)
-    const txtName = ref('')
-    const txtMessage = ref('')
-    const successVisible = ref(false)
-    const successShrink = ref(false)
+const submitted = ref(false)
+const txtName = ref('')
+const txtMessage = ref('')
+const successVisible = ref(false)
+const successShrink = ref(false)
+const chars = ref(0)
+const max = ref(false)
 
-    //example entries and array, would be stored and retrieved in DB in real version
-    const entries = ref([
-      {
-        name: 'Alice',
-        message:
-          'This is a great guestbook! Wow I love it so Im gonna leave a longgg message!! AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAaaaaaaaaaaa HAHA okay Im done!!'
-      },
-      { name: 'pair of pants', message: 'shit sucks' },
-    ])
-    const chars = ref(0)
-    const max = ref(false)
+//example entries and array, would be stored and retrieved in DB in real version
+// const entries = ref([
+//   {
+//     name: 'Alice',
+//     message:
+//       'This is a great guestbook! Wow I love it so Im gonna leave a longgg message!! AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAaaaaaaaaaaa HAHA okay Im done!!',
+//   },
+//   { name: 'pair of pants', message: 'shit sucks' },
+// ])
 
-    // Watch for changes in txtMessage to update character count
-    watch(txtMessage, newValue => {
-      chars.value = newValue.length // Update character count
-      max.value = chars.value >= 150 // Check if max is reached
-    })
+const entries = ref([]) // Store the currently displayed entries
+const currentPage = ref(1) // Page number for pagination
+const entriesPerPage = 3 // Max number of entries to display per page
+const hasMoreEntries = ref(true) // Flag to indicate whether there are more entries to load
+const hasPreviousEntries = ref(false) // Flag to indicate if previous entries exist
 
-    //validates entry (no empty fields)
-    const checkEntry = () => {
-      if (!txtName.value || !txtMessage.value) {
-        return false
-      } else {
-        return true
-      }
+// Function to fetch entries based on the current page
+async function getEntries() {
+  try {
+    const { data, error, count } = await supabase
+      .from('TGuestbook')
+      .select('*', { count: 'exact' })
+      .range(
+        (currentPage.value - 1) * entriesPerPage, // Calculate the range start
+        currentPage.value * entriesPerPage - 1, // Calculate the range end
+      )
+
+    if (error) throw error
+
+    // If it's the first page, we just load the entries; otherwise, we replace the current entries
+    if (currentPage.value === 1) {
+      entries.value = data
+    } else {
+      entries.value = data // Replace old entries with new ones for this page
     }
 
-    // Function to add entry
-    const addEntry = () => {
-      window.scrollTo(0, 0)
-      entries.value.unshift({ name: txtName.value, message: txtMessage.value })
-      txtName.value = ''
-      txtMessage.value = ''
-      successVisible.value = true
-      setTimeout(() => {
-        successShrink.value = true
-      }, 2000) // Show for 2 seconds
-    }
+    // Update flags based on pagination
+    hasMoreEntries.value = currentPage.value * entriesPerPage < count
+    hasPreviousEntries.value = currentPage.value > 1
+  } catch (err) {
+    console.error('Error fetching entries:', err)
+  }
+}
 
-    // Handle form submission
-    const handleSubmit = () => {
-      submitted.value = true
-      //validates entry and returns true if good, then adds with addEntry
-      if (checkEntry()) {
-        addEntry()
-      }
-    }
+// Function to load the next set of entries
+function loadNext() {
+  if (hasMoreEntries.value) {
+    currentPage.value += 1 // Increment the page number
+    getEntries() // Fetch the next set of entries
+  }
+}
 
-    const resetForm = () => {
-      txtName.value = ''
-      txtMessage.value = ''
-      submitted.value = false
-    }
+// Function to load the previous set of entries
+function loadPrevious() {
+  if (hasPreviousEntries.value) {
+    currentPage.value -= 1 // Decrement the page number
+    getEntries() // Fetch the previous set of entries
+  }
+}
 
-    return {
-      txtName,
-      txtMessage,
-      entries,
-      chars,
-      max,
-      addEntry,
-      resetForm,
-      successVisible,
-      handleSubmit,
-      submitted,
-      successShrink,
-    }
-  },
+onMounted(() => {
+  getEntries() // Load initial entries on page load
+})
+
+// Inserting a new entry into the database
+async function insertEntry() {
+  await supabase
+    .from('TGuestbook')
+    .insert({ name: txtName.value, message: txtMessage.value })
+}
+
+// Watch for changes in txtMessage to update character count
+watch(txtMessage, newValue => {
+  chars.value = newValue.length // Update character count
+  max.value = chars.value >= 150 // Check if max character count is reached
+})
+
+// Validate the entry (checking if fields are not empty)
+const checkEntry = () => {
+  return txtName.value && txtMessage.value
+}
+
+// Function to add a new entry
+const addEntry = async () => {
+  window.scrollTo(0, 0) // Scroll to the top
+  await insertEntry() // Insert the new entry into the database
+  txtName.value = '' // Clear the name field
+  txtMessage.value = '' // Clear the message field
+  successVisible.value = true // Show success message
+  getEntries() // Reload entries
+  setTimeout(() => {
+    successShrink.value = true // Hide success message after 2 seconds
+  }, 2000)
+}
+// Handle form submission
+const handleSubmit = () => {
+  submitted.value = true
+  //validates entry and returns true if good, then adds with addEntry
+  if (checkEntry()) {
+    addEntry()
+  }
+}
+
+const resetForm = () => {
+  txtName.value = ''
+  txtMessage.value = ''
+  submitted.value = false
 }
 </script>
+
 <template>
   <div id="guestbook" class="maincontent info">
     <div class="text">
@@ -86,25 +127,53 @@ export default {
     </div>
 
     <div class="guestwrapper" :class="{ shrink: successShrink }">
-
-      <div v-if="successVisible" class="success-message" :class="{ shrink: successShrink }">
+      <div
+        v-if="successVisible"
+        class="success-message"
+        :class="{ shrink: successShrink }"
+      >
         <p><b>Message posted!</b></p>
       </div>
 
-      <form id="GuestForm" v-if="!successVisible" @submit.prevent="handleSubmit" class="form-column">
-        <div id="Formwrapper" class="maincontent guestform" :class="{ shrink: successShrink }">
+      <form
+        id="GuestForm"
+        v-if="!successVisible"
+        @submit.prevent="handleSubmit"
+        class="form-column"
+      >
+        <div
+          id="Formwrapper"
+          class="maincontent guestform"
+          :class="{ shrink: successShrink }"
+        >
+          <h3>What people have said to me:</h3>
           <div class="field name">
             <label>Name: </label>
-            <input v-model="txtName" maxlength="25" class="formfield" type="text" placeholder="Anonymous"
-              :class="{ invalid: !txtName && submitted }" />
+            <input
+              v-model="txtName"
+              maxlength="25"
+              class="formfield"
+              type="text"
+              placeholder="Anonymous"
+              :class="{ invalid: !txtName && submitted }"
+            />
           </div>
 
           <div class="field message">
             <label>Message: </label>
-            <textarea v-model="txtMessage" class="field messagebox" placeholder="Say something nice or mean" rows="4"
-              cols="35" maxlength="150" :class="{ invalid: !txtMessage && submitted }"></textarea>
+            <textarea
+              v-model="txtMessage"
+              class="field messagebox"
+              placeholder="Say something nice or mean"
+              rows="4"
+              cols="35"
+              maxlength="150"
+              :class="{ invalid: !txtMessage && submitted }"
+            ></textarea>
           </div>
-          <label class="field charlimit" :class="{ alert: max }">{{ chars }}/150</label>
+          <label class="field charlimit" :class="{ alert: max }"
+            >{{ chars }}/150</label
+          >
           <div class="field button">
             <button type="button" @click="resetForm">Nevermind!</button>
             <button type="submit">Post!</button>
@@ -112,42 +181,44 @@ export default {
         </div>
       </form>
 
-
-      <div id="messagewrapper" class="message-wrapper" :class="{ shrink: successShrink }">
+      <div
+        id="messagewrapper"
+        class="message-wrapper"
+        :class="{ shrink: successShrink }"
+      >
         <h3>What people have said to me:</h3>
-        <div v-for="(entry, index) in entries" :key="index" class="entry">
+        <div v-for="entry in entries" :key="entry.id" class="entry">
           <div class="messagewrapper holder">
             <div class="displayname">{{ entry.name }} says..</div>
             <div class="msg">{{ entry.message }}</div>
           </div>
+        </div>
+        <div class="field button pag">
+          <button @click="loadPrevious" :disabled="!hasPreviousEntries">
+            <
+          </button>
+          <button @click="loadNext" :disabled="!hasMoreEntries">></button>
         </div>
       </div>
     </div>
   </div>
 </template>
 
-
-
 <style>
 /* mobile-narrow view */
 @media only screen and (max-width: 768px) {
-
   div#GuestForm {
     border-radius: 20px;
   }
 
   div.guestwrapper {
-
     display: grid;
     grid-template-columns: 100%;
-    grid-template-rows: 1.0fr 1.0fr;
-
+    grid-template-rows: 1fr 1fr;
   }
 
-
   div.guestwrapper.shrink {
-
-    grid-template-rows: 0.0fr 1.0fr;
+    grid-template-rows: 0fr 1fr;
     grid-template-columns: 100%;
     gap: 0px;
   }
@@ -165,12 +236,8 @@ export default {
   div#messagewrapper.shrink {
     margin: auto;
     width: 100%;
-
   }
-
-
 }
-
 
 #GuestForm {
   border-radius: 20px;
@@ -180,9 +247,7 @@ export default {
   overflow: clip;
   grid-column: 1;
   background-color: #bae4f944;
-
 }
-
 
 div.field.message textarea {
   height: 200px;
@@ -190,10 +255,11 @@ div.field.message textarea {
 
 #GuestForm div#Formwrapper {
   padding-top: 0;
+  min-height: 590px;
+  padding: 10px;
 }
 
 .guestwrapper {
-
   padding: 10px;
   background-color: rgba(255, 255, 255, 0.645);
   box-shadow: 5px 5px rgba(0, 0, 0, 0.082);
@@ -201,13 +267,15 @@ div.field.message textarea {
   display: grid;
   grid-template-rows: auto;
   grid-template-columns: 50% 50%;
-  transition: grid-template-columns 1s, grid-template-rows 1s, gap 1s;
+  transition:
+    grid-template-columns 1s,
+    grid-template-rows 1s,
+    gap 1s;
 }
 
 .guestwrapper.shrink {
   grid-template-columns: 0% 100%;
   gap: 0px;
-
 }
 
 .success-message {
@@ -221,18 +289,15 @@ div.field.message textarea {
   padding: 10px;
   font-size: 1.2em;
   background-color: #bae4f944;
-
 }
 
 .success-message p {
   padding-top: 30px;
   font-size: calc(1vw + 2vh);
-
 }
 
 .success-message.shrink {
   background-color: #ffffff00;
-
 }
 
 #Formwrapper {
@@ -240,8 +305,6 @@ div.field.message textarea {
   border-radius: 20px;
   gap: 10px;
   padding-top: 20px;
-
-
 }
 
 #Formwrapper .field {
@@ -249,13 +312,10 @@ div.field.message textarea {
   padding: 10px;
 }
 
-
 label {
   font-weight: bold;
   padding-bottom: 2px;
 }
-
-
 
 .alert {
   color: red;
@@ -285,7 +345,6 @@ textarea {
   background-color: rgba(255, 255, 255, 0.5);
   border-radius: 20px;
   box-shadow: 5px 5px rgba(0, 0, 0, 0.082);
-
 }
 
 .invalid {
@@ -293,16 +352,19 @@ textarea {
 }
 
 #messagewrapper {
-
   display: grid;
   padding: 10px;
   padding-bottom: 30px;
   width: 100%;
+  height: 600px;
   border-radius: 20px;
   border-top-left-radius: 0;
   border-bottom-left-radius: 0;
-  transition: width 1s, margin 1s;
+  transition:
+    width 1s,
+    margin 1s;
   background-color: #bae4f944;
+  gap: 10px;
 }
 
 #messagewrapper.shrink {
@@ -312,7 +374,6 @@ textarea {
   border-bottom-left-radius: 20px;
 }
 
-
 #messagewrapper h3 {
   display: inline-grid;
   text-align: center;
@@ -320,7 +381,7 @@ textarea {
 
 .entry {
   display: inline-grid;
-  margin: 10px 0;
+
   min-width: 100%;
   border-radius: 10px;
 }
@@ -329,7 +390,6 @@ div.messagewrapper.holder {
   width: 80%;
   margin: 0 auto;
 }
-
 
 .displayname {
   font-weight: bold;
@@ -369,7 +429,7 @@ button {
   font-size: 20px;
 }
 
-#Formwrapper .field.button {
+.field.button {
   display: inline-grid;
   grid-template-columns: 1fr 1fr;
 }
@@ -384,5 +444,16 @@ button {
 
 div.text {
   background-color: rgba(255, 255, 255, 0.645);
+}
+
+.field.button.pag button {
+  width: 60%;
+}
+.field.button.pag {
+  align-content: end;
+}
+button:disabled {
+  opacity: 0% !important;
+  cursor: default;
 }
 </style>

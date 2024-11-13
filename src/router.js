5eb56899bcc42140f39e6a3ec0c0b668d5694916
JoelleTindall <jtindall222@Gmail.com>
2024-11-13 14:59:42 -0500

    boop

--- src/router.js
+++ src/router.js
@@ -4,32 +4,31 @@ import Projectcarousel from './components/Projectcarousel.vue'
 import Contactform from './components/Contactform.vue'
 import Guestbook from './components/Guestbook.vue'
 const routes = [
-    {
-      path: '/',
-      redirect: '/about',
-    },
-    {
-       path: '/about',
-       name: 'About',
-       component: About,
-    },
-    {
-        path: '/projects',
-        name: 'Projects',
-        component: Projectcarousel,
-    },
-    {
-        path: '/contact',
-        name: 'Contact',
-        component: Contactform,
-    },
-    {
-        path: '/guestbook',
-        name: 'Guestbook',
-        component: Guestbook,
-    }
-  
-  ]
-  
-  const router = createRouter({ history: createWebHistory(), routes })
-  export default router
+  {
+    path: '/',
+    redirect: '/about',
+  },
+  {
+    path: '/about',
+    name: 'About',
+    component: About,
+  },
+  {
+    path: '/projects',
+    name: 'Projects',
+    component: Projectcarousel,
+  },
+  {
+    path: '/contact',
+    name: 'Contact',
+    component: Contactform,
+  },
+  {
+    path: '/guestbook',
+    name: 'Guestbook',
+    component: Guestbook,
+  },
+]
+
+const router = createRouter({ history: createWebHistory(), routes })
+export default router
